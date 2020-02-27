7f05266c44b1920b2a5ed384a42df0b82184c195
Richmond <rbaafi@cnm.edu>
2020-02-27 09:33:47 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/MergeSorter.java
@@ -0,0 +1,44 @@
+package edu.cnm.deepdive;
+
+public class MergeSorter {
+
+  public void sort(int[] data) {
+    sort(data, 0, data.length);
+  }
+
+
+  public void sort(int[] data, int from, int to) {
+    if (to - from <= 1) {
+      return;
+    }
+    int midpoint = (from + to) / 2;
+    sort(data, from, midpoint);
+    sort(data, midpoint, to);
+    merge(data, from, to, midpoint);
+  }
+
+  private void merge(int[] data, int from, int to, int midpoint) {
+    int[] merged = new int[to - from];
+    int leftIndex = from;
+    int rightIndex = midpoint;
+    int mergedIndex = 0;
+    while (leftIndex < midpoint && rightIndex < to) {
+      int leftValue = data[leftIndex];
+      int rightVallue = data[rightIndex];
+      if (leftValue <= rightVallue) {
+        merged[mergedIndex] = leftValue;
+        leftIndex++;
+      } else {
+        merged[mergedIndex] = rightVallue;
+        rightIndex++;
+      }
+      mergedIndex++;
+    }
+    if (leftIndex < midpoint) {
+      System.arraycopy(data, leftIndex, merged, mergedIndex, midpoint - leftIndex);
+    } else {
+      System.arraycopy(data, rightIndex, merged, mergedIndex, to - rightIndex);
+    }
+    System.arraycopy(merged, 0, data, from, merged.length);
+  }
+}
